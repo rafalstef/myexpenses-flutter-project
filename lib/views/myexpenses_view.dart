d34fcd42aebfd22dd9ff3475df0ee7615d861e4d
rafalstef <r.stefanowicz1@gmail.com>
2022-03-13 04:01:06 +0100

    Logout dialog clean up

--- lib/views/myexpenses_view.dart
+++ lib/views/myexpenses_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:myexpenses/constants/routes.dart';
 import 'package:myexpenses/enums/menu_action.dart';
 import 'package:myexpenses/services/auth/auth_service.dart';
+import 'package:myexpenses/utilities/show_logout_dialog.dart';
 
 class MyexpensesView extends StatefulWidget {
   const MyexpensesView({Key? key}) : super(key: key);
@@ -46,28 +47,4 @@ class _MyexpensesViewState extends State<MyexpensesView> {
       body: const Text('Hello World!'),
     );
   }
-}
-
-Future<bool> showLogOutDialog(BuildContext context) {
-  return showDialog<bool>(
-    context: context,
-    builder: (context) {
-      return AlertDialog(
-        title: const Text('Sign out'),
-        content: const Text('Are you sure you want to sign out?'),
-        actions: [
-          TextButton(
-              onPressed: () {
-                Navigator.of(context).pop(false);
-              },
-              child: const Text('Cancel')),
-          TextButton(
-              onPressed: () {
-                Navigator.of(context).pop(true);
-              },
-              child: const Text('Log out')),
-        ],
-      );
-    },
-  ).then((value) => value ?? false);
 }
