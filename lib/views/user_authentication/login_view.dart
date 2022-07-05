12e7b88e5577af7aaa3aaad315860a33473244b2
rafalstef <r.stefanowicz1@gmail.com>
2022-07-05 20:05:54 +0200

    Change UI of login screen

--- lib/views/user_authentication/login_view.dart
+++ lib/views/user_authentication/login_view.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:myexpenses/config/styles/colors/app_colors.dart';
+import 'package:myexpenses/config/styles/text_styles/app_text_styles.dart';
 import 'package:myexpenses/constants/routes.dart';
 import 'package:myexpenses/services/auth/auth_exceptions.dart';
 import 'package:myexpenses/services/auth/auth_service.dart';
+import 'package:myexpenses/utilities/UI_components/buttons/google_button.dart';
+import 'package:myexpenses/utilities/UI_components/buttons/large_buttons.dart';
+import 'package:myexpenses/utilities/UI_components/input_fields/app_text_field.dart';
 import 'package:myexpenses/utilities/dialogs/show_error_dialog.dart';
-import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:myexpenses/utilities/validator/app_forms_validator.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({Key? key}) : super(key: key);
@@ -13,6 +18,7 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
+  final formKey = GlobalKey<FormState>();
   late final TextEditingController _email;
   late final TextEditingController _password;
 
@@ -33,107 +39,177 @@ class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
+      appBar: _buildAppBar(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(height: MediaQuery.of(context).size.height * 0.1),
+                _buildEmailTextField(),
+                const SizedBox(height: 20.0),
+                _buildPasswordTextField(),
+                const SizedBox(height: 30.0),
+                _buildLoginButton(context),
+                const SizedBox(height: 30.0),
+                _buildForgotPasswordButton(context),
+                _buildSignUp(context),
+                const SizedBox(height: 30.0),
+                _buildSignWithGoogle(context),
+              ],
+            ),
+          ),
+        ),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          TextField(
-            textAlign: TextAlign.center,
-            enableSuggestions: false,
-            autocorrect: false,
-            keyboardType: TextInputType.emailAddress,
-            controller: _email,
-            decoration: const InputDecoration(hintText: 'Enter your email'),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      elevation: 0,
+      centerTitle: true,
+      backgroundColor: AppColors.transparent,
+      iconTheme: const IconThemeData(color: AppColors.dark100),
+      title: Text('Login', style: AppTextStyles.title3(AppColors.dark60)),
+    );
+  }
+
+  GoogleButton _buildSignWithGoogle(BuildContext context) {
+    return GoogleButton(
+      text: 'Sign in with Google',
+      onPressed: () async {
+        try {
+          await AuthService.firebase().signInWithGoogle();
+          Navigator.of(context).pushNamedAndRemoveUntil(
+            summaryViewRoute,
+            (route) => false,
+          );
+        } on GenericAuthException {
+          await showErrorDialog(
+            context,
+            'Google Sign-in failed',
+          );
+        }
+      },
+    );
+  }
+
+  Row _buildSignUp(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          "Don't have an account yet?",
+          style: AppTextStyles.regularMedium(AppColors.dark20),
+        ),
+        TextButton(
+          child: const Text(
+            'Sign up',
+            style: TextStyle(
+              decoration: TextDecoration.underline,
+              color: AppColors.violet100,
+              fontSize: 16.0,
+              fontWeight: FontWeight.w500,
+            ),
           ),
-          TextField(
-            textAlign: TextAlign.center,
-            obscureText: true,
-            enableSuggestions: false,
-            autocorrect: false,
-            controller: _password,
-            decoration: const InputDecoration(hintText: 'Enter your password'),
-          ),
-          TextButton(
-            onPressed: () async {
-              final email = _email.text;
-              final password = _password.text;
-              try {
-                await AuthService.firebase().logIn(
-                  email: email,
-                  password: password,
-                );
-                final user = AuthService.firebase().currentUser;
-                if (user?.isEmailVerified ?? false) {
-                  //user's email is verified
-                  Navigator.of(context).pushNamedAndRemoveUntil(
-                    summaryViewRoute,
-                    (route) => false,
-                  );
-                } else {
-                  // user's email is NOT verified
-                  Navigator.of(context).pushNamedAndRemoveUntil(
-                    verifyEmailRoute,
-                    (route) => false,
-                  );
-                }
-              } on UserNotFoundAuthException {
-                await showErrorDialog(
-                  context,
-                  'User not found',
-                );
-              } on WrongPasswordAuthException {
-                await showErrorDialog(
-                  context,
-                  'Wrong credentials',
-                );
-              } on GenericAuthException {
-                await showErrorDialog(
-                  context,
-                  'Authentication error',
-                );
-              }
-            },
-            child: const Text('Login'),
-          ),
-          TextButton(
-            onPressed: () {
+          onPressed: () {
+            Navigator.of(context).pushNamedAndRemoveUntil(
+              registerRoute,
+              (route) => false,
+            );
+          },
+        ),
+      ],
+    );
+  }
+
+  TextButton _buildForgotPasswordButton(BuildContext context) {
+    return TextButton(
+      child: Text(
+        'Forgot password?',
+        style: AppTextStyles.title3(AppColors.violet100),
+      ),
+      onPressed: () {
+        Navigator.of(context).pushNamedAndRemoveUntil(
+          forgotPasswordRoute,
+          (route) => false,
+        );
+      },
+    );
+  }
+
+  Widget _buildLoginButton(BuildContext context) {
+    return LargePrimaryButton(
+      text: 'Login',
+      onPressed: () async {
+        final bool isFormValid = formKey.currentState!.validate();
+        if (isFormValid) {
+          final email = _email.text;
+          final password = _password.text;
+          try {
+            await AuthService.firebase().logIn(
+              email: email,
+              password: password,
+            );
+            final user = AuthService.firebase().currentUser;
+            if (user?.isEmailVerified ?? false) {
+              //user's email is verified
               Navigator.of(context).pushNamedAndRemoveUntil(
-                registerRoute,
+                summaryViewRoute,
                 (route) => false,
               );
-            },
-            child: const Text('Not registered yet? Register here!'),
-          ),
-          TextButton(
-            onPressed: () {
+            } else {
+              // user's email is NOT verified
               Navigator.of(context).pushNamedAndRemoveUntil(
-                forgotPasswordRoute,
+                verifyEmailRoute,
                 (route) => false,
               );
-            },
-            child: const Text('Forgot your password?'),
-          ),
-          SignInButton(
-            Buttons.Google,
-            onPressed: () async {
-              try {
-                await AuthService.firebase().signInWithGoogle();
-                Navigator.of(context).pushNamedAndRemoveUntil(
-                  summaryViewRoute,
-                  (route) => false,
-                );
-              } on GenericAuthException {
-                await showErrorDialog(
-                  context,
-                  'Google Sign-in failed',
-                );
-              }
-            },
-          )
-        ],
-      ),
+            }
+          } on UserNotFoundAuthException {
+            await showErrorDialog(
+              context,
+              'User not found',
+            );
+          } on WrongPasswordAuthException {
+            await showErrorDialog(
+              context,
+              'Wrong credentials',
+            );
+          } on GenericAuthException {
+            await showErrorDialog(
+              context,
+              'Authentication error',
+            );
+          }
+        }
+      },
+    );
+  }
+
+  AppTextFormField _buildPasswordTextField() {
+    return AppTextFormField(
+      textEditingController: _password,
+      textInputType: TextInputType.visiblePassword,
+      textInputAction: TextInputAction.done,
+      obscureText: true,
+      labelText: 'Password',
+      errorText: 'Please enter your password.',
+      validator: (value) =>
+          AppFormsValidator.validateLoginPassword(password: value),
+    );
+  }
+
+  AppTextFormField _buildEmailTextField() {
+    return AppTextFormField(
+      textEditingController: _email,
+      textInputType: TextInputType.emailAddress,
+      textInputAction: TextInputAction.next,
+      labelText: 'Email',
+      errorText: 'Please enter your email address.',
+      validator: (value) => AppFormsValidator.validateLoginEmail(email: value),
     );
   }
 }
